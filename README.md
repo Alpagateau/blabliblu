f91c4377e7c365719ddaa14ecfc9974afc45f291
Alpa <alpaga.ytb@gmail.com>
2022-01-16 23:36:46 +0100

    updated the About and the readme

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
-# flutter_application_1
+# Hello world
 
-A simple flutter project
+Hi, thanks for using this app! It's a joy to know that my app can be useful to someone.
 
 ## Why this project ?
 
-This project is made to help me learn to code applications using flutter.
+This application was created to help people who feel bad to get better. The idea came from an advice given by a friend when I was
+feeling bad. She told me to write down every night three things that made me smile during the day. After doing this for about two
+months, I decided to create this application to help people apply this advice. It also served as my first project to learn how to code 
+applications for the general public.
 
 ## What is this app doing ?
 
-A friend of mine ones said than, writing the good things that happend in your day would actualy help to feel better. So I made this little app to do exactly this. You can write anything you want into this. It will be saved into a file on your phone, wich will never be shared to me. Everything is yours. Its like a secret garden. 
+Every day, you'll receive a notification to remind you to write down the things that made you smile. Only one notification per day is 
+sent. (you can disable them in the settings). When you enter information in the fields provided, the content is saved in a file of your 
+own on your phone. Nothing is sent to anyone. Everything happens on your phone, in private. When you open the Diary, the file is opened
+ and displayed in a convenient way. 
--- lib/aboutPageWidget.dart
+++ lib/aboutPageWidget.dart
@@ -3,20 +3,71 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+//TODO continues the about page
+
 class AboutPage extends StatelessWidget {
   const AboutPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("About")),
-      body: const Text(
-        "This app is dope",
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 21,
-        ),
-      ),
-    );
+        appBar: AppBar(title: const Text("About")),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: const [
+              Text(
+                "Hello world",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+              ),
+              Text(
+                "\nHi, thanks for using this app! It's a joy to know that my app can be useful to someone. \n",
+                style: TextStyle(
+                  fontSize: 18,
+                  wordSpacing: 5,
+                  letterSpacing: 1,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              Divider(),
+              Text(
+                "Why this app ?",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+              ),
+              Text(
+                "\nThis application was created to help people who feel bad to get better. The idea came from an advice given by a friend when I was feeling bad. She told me to write down every night three things that made me smile during the day. After doing this for about two months, I decided to create this application to help people apply this advice. It also served as my first project to learn how to code applications for the general public. \n",
+                style: TextStyle(
+                  fontSize: 18,
+                  wordSpacing: 5,
+                  letterSpacing: 1,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              Divider(),
+              Text(
+                "How does it work ?",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+              ),
+              Text(
+                "\nEvery day, you'll receive a notification to remind you to write down the things that made you smile. Only one notification per day is sent. (you can disable them in the settings). When you enter information in the fields provided, the content is saved in a file of your own on your phone. Nothing is sent to anyone. Everything happens on your phone, in private. When you open the Diary, the file is opened and displayed in a convenient way. ",
+                style: TextStyle(
+                  fontSize: 18,
+                  wordSpacing: 5,
+                  letterSpacing: 1,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+            ],
+          ),
+        ));
   }
 }
